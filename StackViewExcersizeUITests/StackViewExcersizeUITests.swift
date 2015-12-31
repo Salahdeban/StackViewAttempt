d6ef24a2399358300d705e55bdd22cddd108f210
Salahdeban <deban001@gmail.com>
2015-12-31 15:13:26 -0600

    Initial Commit

---
+++ StackViewExcersizeUITests/StackViewExcersizeUITests.swift
@@ -0,0 +1,36 @@
+//
+//  StackViewExcersizeUITests.swift
+//  StackViewExcersizeUITests
+//
+//  Created by Salahaldin Deban on 12/31/15.
+//  Copyright © 2015 Salahaldin Deban. All rights reserved.
+//
+
+import XCTest
+
+class StackViewExcersizeUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
